d9acb5746b797676f43f7617aaf153c082fbd50c
Raoul Wograndl <if21b142@technikum-wien.at>
2023-09-25 09:45:08 +0200

    Add explanations to opengl_context.h

--- GameOpenGL/src/renderer/opengl_context.h
+++ GameOpenGL/src/renderer/opengl_context.h
@@ -2,12 +2,23 @@
 
 struct GLFWwindow;
 
+/// <summary>
+/// A class that holds the OpenGL context. While not necessary for our purpose a general
+/// context class could be created that is inherited by different subclasses (e.g. DirectXContext)
+/// in order to abstract the GLFW window should we decide to replace it later. 
+/// </summary>
 class OpenGLContext
 {
 public:
     OpenGLContext(GLFWwindow* window);
 
+    /// <summary>
+    /// Initializes the context and glad.
+    /// </summary>
     void Initialize();
+    /// <summary>
+    /// Swaps the front and back buffers of the window.
+    /// </summary>
     void SwapBuffers();
 
 private:
